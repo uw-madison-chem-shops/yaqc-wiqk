5c37d8da3f45ae80505036e252f1310d61edf7f2
Monoaxel <jjcoles@wisc.edu>
2023-03-01 12:12:59 -0600

    2023-02-21 Data Writer and Main Window cha

--- yaqc_wiqk/_data_writer.py
+++ yaqc_wiqk/_data_writer.py
@@ -4,6 +4,9 @@ import os
 
 import numpy as np
 import tidy_headers
+import yaqc
+
+from ._timestamp import TimeStamp
 
 from ._timestamp import TimeStamp
 
@@ -11,6 +14,11 @@ from ._timestamp import TimeStamp
 class DataWriter(object):
     def __init__(self, main_window):
         self._main_window = main_window
+        self._valve_0 = yaqc.Client(36000)
+        self._valve_1 = yaqc.Client(36001)
+        self._valve_2 = yaqc.Client(36002)
+        self._valve_3 = yaqc.Client(36003)
+        self._labjack = yaqc.Client(37000)
 
     def create_file(self, procedure: str, procedure_args: dict, start_time: int):
         filename = TimeStamp(at=start_time).path + f" {procedure}.txt"
@@ -22,29 +30,30 @@ class DataWriter(object):
         headers["procedure"] = procedure
         for k, v in procedure_args.items():
             headers[k] = v.get_value()
-        headers["columns"] = ["timestamp",
-                              "valve_0",
-                              "valve_1",
-                              "valve_2",
-                              "valve_3",
-                              "ai0",
-                              "ai1",
-                              "ai2",
-                              "labjack_temperature",
+        headers["columns"] = ["timestamp",  # 0
+                              "valve_0",  # 1
+                              "valve_1",  # 2
+                              "valve_2",  # 3
+                              "valve_3",  # 4
+                              "labjack_temperature",  # 5
+                              "thermocouple",  # 6
+                              "flux",  # 7
+                              "measurement_id"  #8
                               ]
         tidy_headers.write(self.filepath, headers)
 
     def write(self):
         arr = np.empty(9)
+        measured = self._labjack.get_measured()
         arr[0] = time.time()
-        arr[1] = 0
-        arr[2] = 1
-        arr[3] = 2
-        arr[4] = 3
-        arr[5] = 4
-        arr[6] = 5
-        arr[7] = 7
-        arr[8] = 9
+        arr[1] = self._valve_0.get_position()
+        arr[2] = self._valve_1.get_position()
+        arr[3] = self._valve_2.get_position()
+        arr[4] = self._valve_3.get_position()
+        arr[5] = measured["device_temperature"]
+        arr[6] = measured["temperature"]
+        arr[7] = measured["flux"]
+        arr[8] = measured["measurement_id"]
         with open(self.filepath, "a") as f: 
             np.savetxt(f, arr.T, delimiter="\t", newline="\t", fmt="%.6f")
             f.write("\n")
